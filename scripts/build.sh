707cee8d1d76ba1546d255395479d120b27f0ff6
chenlizan <chenlizan@users.noreply.github.com>
2018-11-20 10:09:52 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ if [ "$PLATFORM" != "SunOS" ] ; then
 
   cd $GMP_DEPS
 
-  ./configure --prefix=$BUILD --disable-assembly --enable-shared=no CFLAGS="-fPIC"
+  ./configure --prefix=$BUILD --disable-assembly --disable-shared=no CFLAGS="-fPIC"
 
   make
 
